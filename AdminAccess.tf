81f49a70734d71e6c5cf50a36fefb8da5a0cdf35
Nils Werner <nils.werner@sonarsource.com>
2023-02-21 10:04:56 +0100

    Update code example

--- AdminAccess.tf
+++ AdminAccess.tf
@@ -1,8 +1,13 @@
-resource "aws_security_group" "noncompliant_allow_inbound_ssh" {
+resource "aws_security_group" "allow_inbound_ssh" {
   ingress {
+    description      = "Allow TLS"
     from_port        = 22
     to_port          = 4000
     protocol         = tcp
-    cidr_blocks      = ["0.0.0.0/0"] # Noncompliant
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+   tags = {
+    Name = "allow_tls"
   }
 }
